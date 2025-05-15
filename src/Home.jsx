f4473791e345ff7b7687adf0a97c1b9d7554cdf3
yagocardoso0310 <rm94755@estudante.fieb.edu.br>
2025-05-15 12:42:56 +0000

    pokemon

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+
+const Home = () => {
+
+    return (
+        <div>
+            <img src={poke} />
+            <img src={eevee} />
+        </div>
+    )
+}
+
+export default Home
