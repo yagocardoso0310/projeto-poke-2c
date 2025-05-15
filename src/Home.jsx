a31a2ca69ee424d20d0ef3fb4237e30665948c73
yagocardoso0310 <rm94755@estudante.fieb.edu.br>
2025-05-15 12:54:52 +0000

    pokemon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,29 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
+import './Home.css'
 
 const Home = () => {
 
     return (
         <div>
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
             <img src={poke} />
             <img src={eevee} />
         </div>
