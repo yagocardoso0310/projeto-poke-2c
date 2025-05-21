88b274e48b99efc2ce9aa8b6423445d6db11558e
yagocardoso0310 <rm94755@estudante.fieb.edu.br>
2025-05-21 11:22:47 +0000

    grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,15 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
+import PokeGrid from './PokeGrid'
+import PokeFlex from './PokeFlex'
 
 const Home = () => {
 
     return (
         <div>
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
+            <PokeGrid/>
+            <PokeFlex/>
         </div>
     )
 }
---
+++ src/PokeFlex.jsx
@@ -0,0 +1,29 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './PokeFlex.css'
+
+const PokeFlex = () => {
+
+    return (
+        <div className="PokeFlex">
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+        </div>
+    )
+}
+
+export default PokeFlex
